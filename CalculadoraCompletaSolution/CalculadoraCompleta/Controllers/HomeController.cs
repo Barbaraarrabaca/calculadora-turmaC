d783c47918a99fc5036aebf4181a6b241c89c620
Barbara Arrabaça <aluno19493@ipt.pt>
2018-03-09 20:09:27 +0000

    manipulaçao de algarismos

--- CalculadoraCompletaSolution/CalculadoraCompleta/Controllers/HomeController.cs
+++ CalculadoraCompletaSolution/CalculadoraCompleta/Controllers/HomeController.cs
@@ -11,7 +11,40 @@ namespace CalculadoraCompleta.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            //inicializar o valor do visor
+            ViewBag.visor = 0;
+
             return View();
         }
+
+        // POST: Home
+        [HttpPost]
+        public ActionResult Index(string bt, string visor)
+        {
+
+            //identificar o valor da variavel 'bt'
+            switch (bt)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+
+                    if (visor.Equals("0")) visor = bt;
+                    else visor += bt;
+                    break;
+
+            }
+            //enviar a resposta para o cliente
+            ViewBag.Visor = visor;
+
+            return View();
+        }
+
     }
 }
